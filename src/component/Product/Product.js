b6d35e92503f1d4d54ade4b7f278e9f3efb8406d
Midul-islam-Russell <midulraddy121@gmail.com>
2022-11-15 12:42:38 +0600

    card add

--- src/component/Product/Product.js
+++ src/component/Product/Product.js
@@ -8,10 +8,15 @@ const Product = (props) => {
     return (
         <div className='product'>
             <img src={img} alt="" />
-            <h3>Name : {name}</h3>
-            <p>Price : {price}</p>
-            <p>Quantity : {quantity}</p>
-            <p><span>Seller : {seller}</span></p>
+            <div className='product-info'>
+                <h3>Name : {name}</h3>
+                <p>Price : {price}</p>
+                <p>Quantity : {quantity}</p>
+                <p><span>Seller : {seller}</span></p>
+            </div>
+            <button className='btn-card'>
+                <p>Add to card </p>
+            </button>
         </div>
     );
 };
